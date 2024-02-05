4e9537ff36d454b166d1002310d49f2d24bf7388
Maximiliam999 <maximanuel777@gmail.com>
2024-02-05 14:53:51 +0000

    initial commit

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,57 @@
  */
 
 // Vanilla JavaScript Function
+// function addTwoNumbers(a, b) {
+//     // Code block
+//     return a + b; 
+// }
+// let sum = addTwoNumbers(3,5)
+// console.log(sum)
 
 // Arrow Function With Parameters
+const addTwoNumbers = (a, b) => {
+    // Code block
+    return a + b; 
+}
+let sum = addTwoNumbers(3,5)
+console.log(sum)
 
 // Single Line Arrow Function With Parameters
+// const addTwoNumbers2 = (a, b) => (a + b); also valid
+const addTwoNumbers2 = (a, b) => a + b;
+let sum2 = addTwoNumbers2(6, 4);
+console.log(sum2);
 
 // Implicit Returns
 
+const saySomething = message => console.log(message);
+saySomething('Hello there!!');
+
+const sayHello = () => console.log('hello')
+sayHello();
+
 // Returning Multiple Lines
+
+const returnMultipleLines = () => (
+    `<p>
+      This is a multiline string!
+     </P>
+    `
+)
+console.log(returnMultipleLines());
+
+
+// const averagePoints = (arr, subject) => {
+//     let allPoints = 0;
+//     let sum = 0;
+//     for(let itm of arr) {
+//       if(subject in itm.results) {
+//         allPoints += itm.results[subject];
+//         sum+=1;
+//       }
+//     }
+//     return allPoints / sum;
+// };
+
+// let averageMarks = averagePoints(students, 'english');
+// console.log(averageMarks);
